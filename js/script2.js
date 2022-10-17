60c245c41c72a3773636c870521116939c841fac
Fran Segui <franciscojsegui@gmail.com>
2022-10-16 23:08:47 -0300

    123

--- js/script2.js
+++ js/script2.js
@@ -1,23 +1,26 @@
 var videoArray = [
     {
         titulo: "Funcion Cuadratica",
-        videoInsertado: '//www.youtube.com/embed/769fqyd6_gg',
+        videoInsertado: '//www.youtube.com/embed/6urKrhO0Z-k?list=PLGG_9ReSflXfsyFjgktKqf4Bc4C8xoGmM',
     },
     {
-        titulo: "Funcion lineal",
-        videoInsertado : "//www.youtube.com/embed/SBwAWLyoATA",
+        titulo: "Sistema de Ecuaciones",
+        videoInsertado : "//www.youtube.com/embed/3iBLF1eAfgc?list=PLGG_9ReSflXfsyFjgktKqf4Bc4C8xoGmM",
     },
     {
-        titulo: "Circunferencia",
-        videoInsertado : "//www.youtube.com/embed/BMOUsI8JIaI",
+        titulo: "Ecuaciones exponenciales",
+        videoInsertado : "//www.youtube.com/embed/8ZyfHgm4T4c?list=PLGG_9ReSflXfsyFjgktKqf4Bc4C8xoGmM",
     },
 ]
 const crearVideos = () => {
     const contenedorVideos = document.getElementById("videos");
     for(const video of videoArray){
         contenedorVideos.innerHTML +=
-        `<h2>${video.titulo}</h2>
-        <embed src="${video.videoInsertado}" width="400" height="250" />`
+        `<div id="listaVideos">
+        <h2><strong>${video.titulo}</strong></h2>
+        <embed src="${video.videoInsertado}" width="380" height="auto"/>
+        </div>
+        `
     }
 }   
 crearVideos();
